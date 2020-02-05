cc02039ae0f0a5a536d99f0596516db2e4e1ab18
Mykola Romaniv <mykolaromaniw@gmail.com>
2020-02-05 16:46:55 +0200

    [*] refactor comments

--- src/App/components/Posts/Post/Comments/index.jsx
+++ src/App/components/Posts/Post/Comments/index.jsx
@@ -3,22 +3,19 @@ import React from 'react';
 import classes from './Comments.module.scss';
 
 const comments = props => {
-  let comments = [];
-  if (props.comments && props.comments.length) {
-    if (typeof props.comments === 'string') {
-      comments = props.comments;
-    } else {
-      comments = props.comments.map((comment, index) => {
-        return (
-          <div key={index} className={classes.comment}>
-            {comment}
-          </div>
-        );
-      });
-    }
-  }
-
-  return <div className={classes.commentList}>{comments}</div>;
+  return (
+    <div className={classes.commentList}>
+      {Array.isArray(props.comments)
+        ? (props.comments.map((comment, index) => {
+            return (
+              <div key={index} className={classes.comment}>
+                {comment}
+              </div>
+            );
+          }))
+        : props.comments}
+    </div>
+  );
 };
 
 export default comments;
