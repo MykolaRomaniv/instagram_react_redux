ad63cc4a0d99ac9b471f684789ddf5c10ffd900a
Mykola Romaniv <mykolaromaniw@gmail.com>
2020-02-07 16:50:21 +0200

    fix add post

--- src/App/components/AddPostForm/index.jsx
+++ src/App/components/AddPostForm/index.jsx
@@ -2,19 +2,21 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { toast } from 'react-toastify'
+import CloudUploadIcon from '@material-ui/icons/CloudUpload'
+
 import classes from './AddPostForm.module.scss'
 import * as actions from '../../redux/actions'
 
 const blobToDataURL = async (blob) => {
   try {
     return new Promise((fulfill, reject) => {
-      let reader = new FileReader()
+      const reader = new FileReader()
       reader.onerror = reject
       reader.onload = () => fulfill(reader.result)
       reader.readAsDataURL(blob)
     })
   } catch (error) {
-    toast.error('Can`t load file' + error)
+    toast.error(`Can\`t load file${error}`)
   }
 }
 
@@ -25,13 +27,11 @@ class AddPostForm extends Component {
   }
 
   photoUploadHandler = (event) => {
-    if (event.target.files[0]) {
+    if (event && event.target.files[0]) {
       if (event.target.files[0].size < 100000) {
-        blobToDataURL(event.target.files[0]).then((res) =>
-          this.setState({
-            photo: res,
-          }),
-        )
+        this.setState({
+          photo: event.target.files[0],
+        })
       } else {
         toast.error('Image to large')
       }
@@ -39,12 +39,21 @@ class AddPostForm extends Component {
   }
 
   saveClickedHandler = () => {
-    this.setState({
-      photo: null,
-      description: '',
-    })
-    this.props.actions.addPost(this.state)
-    this.props.saveClicked()
+    if (this.state.photo) {
+      blobToDataURL(this.state.photo).then((res) =>
+        this.props.actions.addPost({
+          photo: res,
+          description: this.state.description,
+        }),
+      )
+      this.setState({
+        photo: null,
+        description: '',
+      })
+      this.props.saveClicked()
+    } else {
+      toast.error('Please add image')
+    }
   }
 
   descriptionChangeHandler = (event) => {
@@ -55,9 +64,16 @@ class AddPostForm extends Component {
 
   render() {
     return (
-      <form className={classes.form} action="#" >
+      <form className={classes.form} action="#">
+        <label htmlFor="photo" className={classes.photoLabel}>
+          <CloudUploadIcon />{' '}
+          <span>
+            {this.state.photo ? this.state.photo.name : 'Choose a photo...'}
+          </span>
+        </label>
         <input
-          className={classes.photo}
+          id="photo"
+          className={classes.photoInput}
           type="file"
           accept="image/*,image/jpeg"
           onChange={this.photoUploadHandler}
